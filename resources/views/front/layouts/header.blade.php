68f922b98843ecfbe459691c4c9468937905db7d
Nizomiddinkhoja <nizomiddinkhoja@gmail.com>
2021-04-10 11:11:27 +0500

    - refactoring header file

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -1,12 +1,11 @@
 <header id="header-part" class="header-four">
-
     <div class="navigation">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg">
                         <a class="navbar-brand" href="{{route('index')}}">
-                            <img src="/front/images/logo.svg" alt="Logo" height=70px width=auto>
+                            <img src="{{asset('/front/images/logo.svg')}}" alt="Logo" height=70px width=auto>
                         </a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -15,7 +14,6 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-
                         <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item">
@@ -30,41 +28,34 @@
                                 <li class="nav-item">
                                     <a href="{{route('mmt')}}">ММТ</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a href="{{route('info')}}">БА ВОЛИДАЙН</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="{{route('info')}}">БА МАКТАБ</a>
-                                </li>
 
-                            </ul>
-                        </div>
-                        <div class="right-icon text-right">
-                            <ul>
+                                <li class="nav-item">
+                                    <a class="">МАЪЛУМОТ</a>
+                                    <ul class="sub-menu">
+                                        <li><a class="" href="{{route('info', 'for-pupil')}}">БА ХОНАНДА</a></li>
+                                        <li><a class="" href="{{route('info', 'for-school')}}">БА МАКТАБ</a></li>
+                                        <li><a class="" href="{{route('info', 'for-teacher')}}">БА МУАЛЛИМ</a></li>
+                                        <li><a class="" href="{{route('info', 'for-parent')}}">БА ВОЛИДАЙН</a></li>
+                                    </ul>
+                                </li>
                                 <li><a href="javascript:void(0)" id="search"><i class="fa fa-search"></i></a></li>
-                                <!-- <li><a href="#"><i class="fa fa-shopping-bag"></i><span>0</span></a></li> -->
+                                <li><a href="{{route('front-login')}}"><i class="fa fa-user"></i> </a></li>
                             </ul>
                         </div>
-                        <!-- right icon -->
                     </nav>
-                    <!-- nav -->
                 </div>
             </div>
-            <!-- row -->
         </div>
-        <!-- container -->
     </div>
 </header>
-
-
 <div class="search-box">
     <div class="search-form">
         <div class="closebtn">
             <span></span>
             <span></span>
         </div>
-        <form action="#">
-            <input type="text" placeholder="Ҷустуҷӯ кардан">
+        <form action="{{route('search')}}">
+            <input type="text" name="text" required placeholder="Ҷустуҷӯ кардан">
             <button><i class="fa fa-search"></i></button>
         </form>
     </div>
